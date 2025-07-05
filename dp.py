33942a236131b7564f9e8db99e32fc7d785cf943
Muhammed Bilal M N <muhammedbilalmn585@gmail.com>
2025-07-05 11:54:07 +0530

    more files

--- dp.py
+++ dp.py
@@ -14,10 +14,22 @@ def fib_dp(n):
     hash_map[n] = fib_dp(n-1) + fib_dp(n-2)
     return hash_map[n]
 
+def new_dp(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    a,b = 0,1
+    for i in range(n-1):
+        c=a+b
+        a=b
+        b=c
+    return c
 import datetime
 start = datetime.datetime.now()
 
-print(fib_dp(300))
+# print(fib_dp(300))
+print(new_dp(1000))
 
 end = datetime.datetime.now()
 print(end-start)
